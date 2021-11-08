8329c5a1334a96360e79f30e07a292d462cb0b50
dogukanteber <dogukanteber1@hotmail.com>
2021-11-08 22:27:58 +0300

    Refactoring

--- include/s_string.h
+++ include/s_string.h
@@ -52,7 +52,7 @@ char* string_reverse(const char*);
 *	@attention The caller of this function have to keep the returned value in a variable and after done using it, s/he must free the memory.
 *
 */
-char* string_upper(char*); // review
+char* string_upper(char*);
 
 /**
 *	@brief Convert each character to lowercase
@@ -66,13 +66,13 @@ char* string_upper(char*); // review
 *	@attention The caller of this function have to keep the returned value in a variable and after done using it, s/he must free the memory.
 *
 */
-char* string_lower(char*); // review
+char* string_lower(char*);
 
 /**
 *	@brief Checks if the given string starts with the given prefix
 *
 *	@param const char* string
-*   @param const char* prefix
+*   @param const char* sub_string
 *
 *	@returns Returns 1 if the string starts with the prefix, 0 otherwise
 *
@@ -83,7 +83,7 @@ int string_starts_with(const char*, const char*);
 *	@brief Checks if the given string ends with the given postfix
 *
 *	@param const char* string
-*   @param const char* postfix
+*   @param const char* sub_string
 *
 *	@returns Returns 1 if the string ends with the postfix, 0 otherwise
 *
