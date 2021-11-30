c6b6caad55e2c1ac34285e627aea646ff29b0972
dogukanteber <dogukanteber1@hotmail.com>
2021-11-30 23:13:37 +0300

    Initial commit

---
+++ test/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "test.h"
+
+int main(int argc, char const *argv[]) {
+    dynamic_array_test();
+    stack_test();
+    queue_test();
+    s_string_test();
+
+    return 0;
+}
